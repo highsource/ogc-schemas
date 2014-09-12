c14f43c499423c42f95e5455260107e27ce9ea38
highsource <aleksei.valikov@gmail.com>
2014-09-12 17:25:45 +0200

    Issue #10 - KML works now.

--- scripts/tests/KML/2.2.0/KML_2_2_0.js
+++ scripts/tests/KML/2.2.0/KML_2_2_0.js
@@ -3,8 +3,10 @@ var Atom_1_0 = require('../../../ogc-schemas').Atom_1_0;
 var xAL_2_0 = require('../../../ogc-schemas').xAL_2_0;
 var KML_2_2_0 = require('../../../ogc-schemas').KML_2_2_0;
 
-var roundtrip = require('../../roundtrip').roundtrip;
+var roundtrips = require('../../roundtrip').roundtrips;
+
 var mappings = [Atom_1_0, xAL_2_0, KML_2_2_0];
+
 module.exports = {
 	"Context": function(test) {
 		var context = new Jsonix.Context(mappings);
@@ -14,11 +16,9 @@ module.exports = {
 		var context =  new Jsonix.Context([Atom_1_0, xAL_2_0, KML_2_2_0]);
 		var unmarshaller = context.createUnmarshaller();
 		unmarshaller.unmarshalFile("tests/KML/2.2.0/states.kml", function(result) {
-			test.equal("DOCS", result.value.featureTypeList.featureType[0].title);
+			test.equal("Alabama", result.value.abstractFeatureGroup.value.abstractFeatureGroup[0].value.name);
 			test.done();
 		});
 	},
-	"Roundtrips" : {
-		"states.kml" : function(test) {roundtrip(test, mappings, "tests/KML/2.2.0/states.kml");}
-	}
+	"Roundtrips" : roundtrips(mappings, __dirname)
 };
