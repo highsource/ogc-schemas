e9ff22b8040ab58351734a4345930dfd59cdbb82
highsource <aleksei.valikov@gmail.com>
2014-09-28 12:33:35 +0200

    Updated schemas.

--- schemas/src/main/resources/create-patch.bat
+++ schemas/src/main/resources/create-patch.bat
@@ -1,2 +1,1 @@
-rem git diff --patch --no-prefix --relative 57ccb505d989f23a7dead26458d01afeed4180d9 -- ogc > ../../../src/main/patches/ogc.patch
-git diff --patch --no-prefix genesis master -- ogc > 2.patch
+git diff --patch --no-prefix --relative genesis master -- ogc > ../../../src/main/patches/ogc.patch
