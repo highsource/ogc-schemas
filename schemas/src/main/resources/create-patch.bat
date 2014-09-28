36c73917fbac10093dbc4254bfb0bf3e1852d3ea
highsource <aleksei.valikov@gmail.com>
2014-09-28 11:54:51 +0200

    Working on the patch batch.

--- schemas/src/main/resources/create-patch.bat
+++ schemas/src/main/resources/create-patch.bat
@@ -1,1 +1,2 @@
-git diff --patch --no-prefix --relative 57ccb505d989f23a7dead26458d01afeed4180d9 -- ogc > ../../../src/main/patches/ogc.patch
+rem git diff --patch --no-prefix --relative 57ccb505d989f23a7dead26458d01afeed4180d9 -- ogc > ../../../src/main/patches/ogc.patch
+git diff --patch --no-prefix genesis master -- ogc > 2.patch
