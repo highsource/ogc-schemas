155b9950622696bb962306e90c1e10db24798cf8
highsource <aleksei.valikov@gmail.com>
2014-12-13 20:30:09 +0100

    Issue #33. A demonstrating test case.

--- scripts/tests/SLD/1.0.0/SLD_1_0_0.js
+++ scripts/tests/SLD/1.0.0/SLD_1_0_0.js
@@ -11,11 +11,20 @@ module.exports = {
 		var context = new Jsonix.Context(mappings);
 		test.done();
         },
-	"Example" : function(test) {
+	"example-sld.xml" : function(test) {
 		var context =  new Jsonix.Context([XLink_1_0, Filter_1_0_0, GML_2_1_2, SLD_1_0_0]);
 		var unmarshaller = context.createUnmarshaller();
 		unmarshaller.unmarshalFile("tests/SLD/1.0.0/example-sld.xml", function(result) {
 			test.equal("GEOSYM", result.value.namedLayerOrUserLayer[0].namedStyleOrUserStyle[0].name);
+			test.done();
+		});
+	},
+	"example-sld-propertyname.xml" : function(test) {
+		var context =  new Jsonix.Context([XLink_1_0, Filter_1_0_0, GML_2_1_2, SLD_1_0_0]);
+		var unmarshaller = context.createUnmarshaller();
+		unmarshaller.unmarshalFile("tests/SLD/1.0.0/example-sld-propertyname.xml", function(result) {
+			console.log();
+			test.equal('name', result.value.namedLayerOrUserLayer[0].namedStyleOrUserStyle[0].featureTypeStyle[0].rule[0].symbolizer[1].value.label.content[0].value.content);
 			test.done();
 		});
 	},
