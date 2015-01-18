83e0176baa881c79a58cc502269e2fefd1b43489
highsource <aleksei.valikov@gmail.com>
2015-01-18 22:00:35 +0100

    Issue #42.

---
+++ scripts/tests/CSW/2.0.2/CSW_2_0_2_Full.js
@@ -0,0 +1,28 @@
+var Jsonix = require('jsonix').Jsonix;
+var XLink_1_0 = require('../../../lib/CSW_2_0_2_Full').XLink_1_0;
+var OWS_1_0_0 = require('../../../lib/CSW_2_0_2_Full').OWS_1_0_0;
+var DC_1_1 = require('../../../lib/CSW_2_0_2_Full').DC_1_1;
+var DCT = require('../../../lib/CSW_2_0_2_Full').DCT;
+var CSW_2_0_2 = require('../../../lib/CSW_2_0_2_Full').CSW_2_0_2;
+
+var roundtrip = require('../../roundtrip').roundtrip;
+
+var mappings = [OWS_1_0_0, DC_1_1, DCT, XLink_1_0, CSW_2_0_2];
+
+module.exports = {
+	"Context": function(test) {
+		var context = new Jsonix.Context(mappings);
+		test.done();
+        },
+	"Example" : function(test) {
+		var context =  new Jsonix.Context(mappings);
+		var unmarshaller = context.createUnmarshaller();
+		unmarshaller.unmarshalFile("tests/CSW/2.0.2/Clause_6.3.3_Example01_Response.xml", function(result) {
+			test.equal(-108.44, result.value.boundingBox[0].value.lowerCorner[0]);
+			test.done();
+		});
+	},
+	"Roundtrips" : {
+		"Clause_6.3.3_Example01_Response.xml" : function(test) {roundtrip(test, mappings, "tests/CSW/2.0.2/Clause_6.3.3_Example01_Response.xml");}
+	}
+};
--- scripts/tests/CSW/CSW.js
+++ scripts/tests/CSW/CSW.js
@@ -1,6 +1,7 @@
 module.exports = 
 {
 	"CSW": {
-		"CSW_2_0_2" : require('./2.0.2/CSW_2_0_2')
+		"CSW_2_0_2" : require('./2.0.2/CSW_2_0_2'),
+		"CSW_2_0_2_Full" : require('./2.0.2/CSW_2_0_2_Full')
 	}
 };
