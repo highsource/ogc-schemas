56162104140055365cbb862c2e6bb1b4272fd5ff
highsource <aleksei.valikov@gmail.com>
2015-02-07 20:35:20 +0100

    CLI compilation demos.

---
+++ demos/filter/1.1.0/generate-sources.bat
@@ -0,0 +1,7 @@
+java -jar lib/jsonix-schema-compiler-full-2.3.1.jar^
+ -compact^
+ -catalog schemas/catalog.cat^
+ schemas/ogc/filter/1.1.0/filterAll.xsd^
+ -b schemas^
+ -d mappings
+ 
---
+++ demos/wms/1.3.0/generate-sources.bat
@@ -0,0 +1,4 @@
+java -jar lib/jsonix-schema-compiler-full-2.3.1.jar^
+ -catalog schemas/catalog.cat^
+ schemas/ogc/wms/1.3.0^
+ -b schemas -d mappings
