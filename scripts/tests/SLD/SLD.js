28b39c4a07099ec46a58a02cd34eea4370a13c5e
highsource <aleksei.valikov@gmail.com>
2015-04-15 22:58:13 +0200

    Issue #87.

--- scripts/tests/SLD/SLD.js
+++ scripts/tests/SLD/SLD.js
@@ -1,6 +1,7 @@
 module.exports = 
 {
 	"SLD": {
-		"SLD_1_0_0" : require('./1.0.0/SLD_1_0_0')
+		"SLD_1_0_0" : require('./1.0.0/SLD_1_0_0'),
+		"SLD_1_0_0_GeoServer" : require('./1.0.0-geoserver/SLD_1_0_0_GeoServer')
 	}
 };
