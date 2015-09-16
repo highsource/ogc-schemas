41fb53f4fb6896673b8f71ca54c85e76f23feb5c
highsource <aleksei.valikov@gmail.com>
2015-09-16 16:21:12 +0200

    Issue #109. Added schemas and first patch.

--- schemas/src/main/resources/create-patch.bat
+++ schemas/src/main/resources/create-patch.bat
@@ -1,2 +1,3 @@
 diff -urN ../resources-original/ogc ogc > ../../../src/main/patches/ogc.patch
 diff -urN ../resources-original/geoserver geoserver > ../../../src/main/patches/geoserver.patch
+diff -urN ../resources-original/iso iso > ../../../src/main/patches/iso.patch
