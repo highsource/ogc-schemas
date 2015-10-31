a054b5e4124e33357788e2e4386851ac8141667d
highsource <aleksei.valikov@gmail.com>
2015-10-31 10:49:09 +0100

    Issue #125.

---
+++ scripts/tests/SensorML/1.0.0/SensorML_1_0_0.js
@@ -0,0 +1,28 @@
+var Jsonix = require('jsonix').Jsonix;
+var XLink_1_0 = require('w3c-schemas').XLink_1_0;
+var SMIL_2_0 = require('../../../ogc-schemas').SMIL_2_0;
+var SMIL_2_0_Language = require('../../../ogc-schemas').SMIL_2_0_Language;
+var GML_3_1_1 = require('../../../ogc-schemas').GML_3_1_1;
+var IC_2_0 = require('../../../ogc-schemas').IC_2_0;
+var SWE_1_0_0 = require('../../../ogc-schemas').SWE_1_0_0;
+var SensorML_1_0_0 = require('../../../ogc-schemas').SensorML_1_0_0;
+
+var roundtrips = require('../../roundtrip').roundtrips;
+
+var mappings = [XLink_1_0, SMIL_2_0, SMIL_2_0_Language, GML_3_1_1, IC_2_0, SWE_1_0_0, SensorML_1_0_0];
+
+module.exports = {
+	"Context": function(test) {
+		var context = new Jsonix.Context(mappings);
+		test.done();
+        },
+	"Example" : function(test) {
+		var context =  new Jsonix.Context(mappings);
+		var unmarshaller = context.createUnmarshaller();
+		unmarshaller.unmarshalFile(__dirname +"/SML-DescribeSensor-Network.xml", function(result) {
+			test.equal(32.7, result.value.member[0].process.value.boundedBy.envelope.value.lowerCorner.value[0]);
+			test.done();
+		});
+	},
+	"Roundtrips" : roundtrips(mappings, __dirname)
+};
--- scripts/tests/SensorML/SensorML.js
+++ scripts/tests/SensorML/SensorML.js
@@ -1,6 +1,7 @@
 module.exports = 
 {
 	"SensorML": {
+		"SensorML_1_0_0" : require('./1.0.0/SensorML_1_0_0'),
 		"SensorML_1_0_1" : require('./1.0.1/SensorML_1_0_1'),
 		"SensorML_2_0" : require('./2.0/SensorML_2_0')
 	}
