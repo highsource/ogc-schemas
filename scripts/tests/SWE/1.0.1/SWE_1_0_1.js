67e1fb660dd2c6fd6804824eb293b9a75969869b
highsource <aleksei.valikov@gmail.com>
2015-10-31 10:48:49 +0100

    Issue #127.

---
+++ scripts/tests/SWE/1.0.1/SWE_1_0_1.js
@@ -0,0 +1,18 @@
+var Jsonix = require('jsonix').Jsonix;
+var XLink_1_0 = require('w3c-schemas').XLink_1_0;
+var SMIL_2_0 = require('../../../ogc-schemas').SMIL_2_0;
+var SMIL_2_0_Language = require('../../../ogc-schemas').SMIL_2_0_Language;
+var GML_3_1_1 = require('../../../ogc-schemas').GML_3_1_1;
+var SWE_1_0_1 = require('../../../ogc-schemas').SWE_1_0_1;
+
+var roundtrips = require('../../roundtrip').roundtrips;
+
+var roundtrip = require('../../roundtrip').roundtrip;
+var mappings = [XLink_1_0, SMIL_2_0, SMIL_2_0_Language, GML_3_1_1, SWE_1_0_1];
+module.exports = {
+	"Context": function(test) {
+		var context = new Jsonix.Context(mappings);
+		test.done();
+        },
+	"Roundtrips" : roundtrips(mappings, __dirname)
+};
--- scripts/tests/SWE/SWE.js
+++ scripts/tests/SWE/SWE.js
@@ -1,6 +1,8 @@
 module.exports = 
 {
 	"SWE": {
+		"SWE_1_0_0" : require('./1.0.0/SWE_1_0_0'),
+		"SWE_1_0_1" : require('./1.0.1/SWE_1_0_1'),
 		"SWE_2_0" : require('./2.0/SWE_2_0')
 	}
 };
