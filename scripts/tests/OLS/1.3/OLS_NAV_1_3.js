61dcefb4e15afcc4e50c83fe6c089b8fd4d12b2a
highsource <aleksei.valikov@gmail.com>
2015-10-31 10:49:59 +0100

    Issues #120, #121, #122.

---
+++ scripts/tests/OLS/1.3/OLS_NAV_1_3.js
@@ -0,0 +1,19 @@
+var Jsonix = require('jsonix').Jsonix;
+var XLink_1_0 = require('w3c-schemas').XLink_1_0;
+var SMIL_2_0 = require('../../../ogc-schemas').SMIL_2_0;
+var SMIL_2_0_Language = require('../../../ogc-schemas').SMIL_2_0_Language;
+var GML_3_1_1 = require('../../../ogc-schemas').GML_3_1_1;
+var OLS_1_2 = require('../../../ogc-schemas').OLS_1_2;
+var OLS_NAV_1_3 = require('../../../ogc-schemas').OLS_NAV_1_3;
+
+var roundtrips = require('../../roundtrip').roundtrips;
+
+var mappings = [XLink_1_0, SMIL_2_0, SMIL_2_0_Language, GML_3_1_1, OLS_1_2, OLS_NAV_1_3];
+
+module.exports = {
+	"Context": function(test) {
+		var context = new Jsonix.Context(mappings);
+		test.done();
+        },
+	"Roundtrips" : roundtrips(mappings, __dirname)
+};
---
+++ scripts/tests/OLS/OLS.js
@@ -0,0 +1,8 @@
+module.exports = 
+{
+	"OLS": {
+		"OLS_1_1_0" : require('./1.1.0/OLS_1_1_0'),
+		"OLS_1_2" : require('./1.2/OLS_1_2'),
+		"OLS_NAV_1_3" : require('./1.3/OLS_NAV_1_3')
+	}
+};
