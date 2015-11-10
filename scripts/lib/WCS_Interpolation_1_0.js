bb70dbc0321b056139a740c189bb88cf35415eba
highsource <aleksei.valikov@gmail.com>
2015-11-10 23:27:51 +0100

    Issue #130.

---
+++ scripts/lib/WCS_Interpolation_1_0.js
@@ -0,0 +1,57 @@
+var WCS_Interpolation_1_0_Module_Factory = function () {
+  var WCS_Interpolation_1_0 = {
+    n: 'WCS_Interpolation_1_0',
+    dens: 'http:\/\/www.opengis.net\/WCS_service-extension_interpolation\/1.0',
+    tis: [{
+        ln: 'InterpolationMetadataType',
+        ps: [{
+            n: 'interpolationSupported',
+            mno: 0,
+            col: true
+          }]
+      }, {
+        ln: 'InterpolationType',
+        ps: [{
+            n: 'globalInterpolation',
+            rq: true
+          }, {
+            n: 'interpolationPerAxis',
+            mno: 0,
+            col: true,
+            en: 'InterpolationPerAxis',
+            ti: '.InterpolationPerAxisType'
+          }]
+      }, {
+        ln: 'InterpolationPerAxisType',
+        ps: [{
+            n: 'axis',
+            rq: true
+          }, {
+            n: 'interpolationMethod',
+            rq: true
+          }]
+      }],
+    eis: [{
+        en: 'InterpolationMetadata',
+        ti: '.InterpolationMetadataType'
+      }, {
+        en: 'Interpolation',
+        ti: '.InterpolationType'
+      }]
+  };
+  return {
+    WCS_Interpolation_1_0: WCS_Interpolation_1_0
+  };
+};
+if (typeof define === 'function' && define.amd) {
+  define([], WCS_Interpolation_1_0_Module_Factory);
+}
+else {
+  var WCS_Interpolation_1_0_Module = WCS_Interpolation_1_0_Module_Factory();
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports.WCS_Interpolation_1_0 = WCS_Interpolation_1_0_Module.WCS_Interpolation_1_0;
+  }
+  else {
+    var WCS_Interpolation_1_0 = WCS_Interpolation_1_0_Module.WCS_Interpolation_1_0;
+  }
+}
