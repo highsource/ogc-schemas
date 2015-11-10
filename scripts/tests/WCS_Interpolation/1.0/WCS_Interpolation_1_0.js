6fe0c053f5514e8ea3647aa24617bf170a929755
highsource <aleksei.valikov@gmail.com>
2015-11-10 23:28:59 +0100

    Issue #130.

---
+++ scripts/tests/WCS_Interpolation/1.0/WCS_Interpolation_1_0.js
@@ -0,0 +1,21 @@
+var Jsonix = require('jsonix').Jsonix;
+var XLink_1_0 = require('w3c-schemas').XLink_1_0;
+var GML_3_2_1 = require('../../../ogc-schemas').GML_3_2_1;
+var SWE_2_0 = require('../../../ogc-schemas').SWE_2_0;
+var GMLCOV_1_0 = require('../../../ogc-schemas').GMLCOV_1_0;
+var WCS_2_0 = require('../../../ogc-schemas').WCS_2_0;
+var OWS_2_0 = require('../../../ogc-schemas').OWS_2_0;
+var WCS_Interpolation_1_0 = require('../../../ogc-schemas').WCS_Interpolation_1_0;
+
+var roundtrips = require('../../roundtrip').roundtrips;
+
+var roundtrip = require('../../roundtrip').roundtrip;
+var mappings = [XLink_1_0, GML_3_2_1, SWE_2_0, GMLCOV_1_0, OWS_2_0, WCS_2_0, WCS_Interpolation_1_0];
+
+module.exports = {
+	"Context": function(test) {
+		var context = new Jsonix.Context(mappings);
+		test.done();
+        },
+	"Roundtrips" : roundtrips(mappings, __dirname)
+};
---
+++ scripts/tests/WCS_Interpolation/WCS_Interpolation.js
@@ -0,0 +1,6 @@
+module.exports = 
+{
+	"WCS_Interpolation": {
+		"WCS_Interpolation_1_0" : require('./1.0/WCS_Interpolation_1_0')
+	}
+};
