babf2ff026ef39f1d9fad1f816b3b2d99d2038a3
highsource <aleksei.valikov@gmail.com>
2015-11-10 23:25:37 +0100

    Fixes #131.

---
+++ scripts/lib/WCS_Processing_2_0.js
@@ -0,0 +1,40 @@
+var WCS_Processing_2_0_Module_Factory = function () {
+  var WCS_Processing_2_0 = {
+    n: 'WCS_Processing_2_0',
+    dens: 'http:\/\/www.opengis.net\/wcs_service-extension_processing\/2.0',
+    deps: ['WCS_2_0'],
+    tis: [{
+        ln: 'ProcessCoveragesType',
+        bti: 'WCS_2_0.RequestBaseType',
+        ps: [{
+            n: 'query',
+            rq: true
+          }, {
+            n: 'any',
+            mno: 0,
+            col: true,
+            mx: false,
+            t: 'ae'
+          }]
+      }],
+    eis: [{
+        en: 'ProcessCoverages',
+        ti: '.ProcessCoveragesType'
+      }]
+  };
+  return {
+    WCS_Processing_2_0: WCS_Processing_2_0
+  };
+};
+if (typeof define === 'function' && define.amd) {
+  define([], WCS_Processing_2_0_Module_Factory);
+}
+else {
+  var WCS_Processing_2_0_Module = WCS_Processing_2_0_Module_Factory();
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports.WCS_Processing_2_0 = WCS_Processing_2_0_Module.WCS_Processing_2_0;
+  }
+  else {
+    var WCS_Processing_2_0 = WCS_Processing_2_0_Module.WCS_Processing_2_0;
+  }
+}
---
+++ scripts/tests/WCS_Processing/WCS_Processing.js
@@ -0,0 +1,6 @@
+module.exports = 
+{
+	"WCS_Processing": {
+		"WCS_Processing_2_0" : require('./2.0/WCS_Processing_2_0')
+	}
+};
