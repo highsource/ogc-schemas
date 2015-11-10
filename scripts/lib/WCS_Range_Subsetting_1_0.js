c72ed153d9bf495562a8843381621566c3351661
highsource <aleksei.valikov@gmail.com>
2015-11-10 23:24:48 +0100

    Fixes #132.

---
+++ scripts/lib/WCS_Range_Subsetting_1_0.js
@@ -0,0 +1,56 @@
+var WCS_Range_Subsetting_1_0_Module_Factory = function () {
+  var WCS_Range_Subsetting_1_0 = {
+    n: 'WCS_Range_Subsetting_1_0',
+    dens: 'http:\/\/www.opengis.net\/wcs\/range-subsetting\/1.0',
+    tis: [{
+        ln: 'RangeItemType',
+        ps: [{
+            n: 'rangeComponent',
+            rq: true,
+            en: 'RangeComponent'
+          }, {
+            n: 'rangeInterval',
+            rq: true,
+            en: 'RangeInterval',
+            ti: '.RangeIntervalType'
+          }]
+      }, {
+        ln: 'RangeSubsetType',
+        ps: [{
+            n: 'rangeItem',
+            rq: true,
+            col: true,
+            en: 'RangeItem',
+            ti: '.RangeItemType'
+          }]
+      }, {
+        ln: 'RangeIntervalType',
+        ps: [{
+            n: 'startComponent',
+            rq: true
+          }, {
+            n: 'endComponent',
+            rq: true
+          }]
+      }],
+    eis: [{
+        en: 'RangeSubset',
+        ti: '.RangeSubsetType'
+      }]
+  };
+  return {
+    WCS_Range_Subsetting_1_0: WCS_Range_Subsetting_1_0
+  };
+};
+if (typeof define === 'function' && define.amd) {
+  define([], WCS_Range_Subsetting_1_0_Module_Factory);
+}
+else {
+  var WCS_Range_Subsetting_1_0_Module = WCS_Range_Subsetting_1_0_Module_Factory();
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports.WCS_Range_Subsetting_1_0 = WCS_Range_Subsetting_1_0_Module.WCS_Range_Subsetting_1_0;
+  }
+  else {
+    var WCS_Range_Subsetting_1_0 = WCS_Range_Subsetting_1_0_Module.WCS_Range_Subsetting_1_0;
+  }
+}
---
+++ scripts/tests/WCS_Range_Subsetting/WCS_Range_Subsetting.js
@@ -0,0 +1,6 @@
+module.exports = 
+{
+	"WCS_Range_Subsetting": {
+		"WCS_Range_Subsetting_1_0" : require('./1.0/WCS_Range_Subsetting_1_0')
+	}
+};
