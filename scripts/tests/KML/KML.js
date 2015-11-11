4254dc2a1763640edc61061f82e5870ab25999ed
highsource <aleksei.valikov@gmail.com>
2015-11-11 22:56:39 +0100

    Issues #134, #135.

--- scripts/tests/KML/KML.js
+++ scripts/tests/KML/KML.js
@@ -1,6 +1,8 @@
 module.exports = 
 {
 	"KML": {
-		"KML_2_2_0" : require('./2.2.0/KML_2_2_0')
+		"KML_2_1" : require('./2.1/KML_2_1'),
+		"KML_2_2_0" : require('./2.2.0/KML_2_2_0'),
+		"KML_2_3" : require('./2.3/KML_2_3')
 	}
 };
