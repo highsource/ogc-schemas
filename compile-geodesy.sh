cd6238f11af6694272009f2c5c2823d4bc01db05
Lazar Bodor <lazar.bodor@ga.gov.au>
2016-02-17 10:40:56 +1100

    Bump geodesyml dependency to 0.2.2

--- compile-geodesy.sh
+++ compile-geodesy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-mvn clean package -DskipTests=true -pl schemas,iso19139/20070417,gml/3.2.1,geodesyml/0.2.1
+mvn clean install -DskipTests=true -pl schemas,iso19139/20070417,gml/3.2.1,om/2.0,geodesyml/0.2.2
