2b21fc123e2fec718029d915e404c2307c755333
Brooke Smith <brooke.smith@ga.gov.au>
2016-03-31 17:00:59 +1100

    Bump Geodesyml version from 0.2.2 to 0.3

--- compile-geodesy.sh
+++ compile-geodesy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-mvn clean install -DskipTests=true -pl schemas,iso19139/20070417,gml/3.2.1,om/2.0,geodesyml/0.2.2
+mvn clean install -DskipTests=true -pl schemas,iso19139/20070417,gml/3.2.1,om/2.0,geodesyml/0.3
