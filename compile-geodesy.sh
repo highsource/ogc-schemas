cd9b95309081e4400dd6c200ccc54eada4effb54
lbodor <lazar.bodor@gmail.com>
2016-04-02 10:02:26 +1100

    Add missing dependencies to compile-geodesy.sh

--- compile-geodesy.sh
+++ compile-geodesy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-mvn clean install -DskipTests=true -pl schemas,iso19139/20070417,gml/3.2.1,om/2.0,geodesyml/0.3
+mvn clean install -DskipTests=true -pl .,schema-parent,schemas,iso19139/20070417,gml/3.2.1,om/2.0,geodesyml/0.3
