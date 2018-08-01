3e7883465121fd75cabed640ae34754c905d9910
charlesmoore99 <charlesmoore99@gmail.com>
2018-08-01 14:29:54 -0400

    A unit test demonstrating a flaw in the xcj generated bindings for SOS 2 GetObservation.
    GetObservationType.TemporalFilter should accept args of type JAXBElement<? extends TemporalOpType>, but instead only accepts JAXBElement<TemporalOpType>

---
+++ sos/2.0/src/test/java/net/opengis/sos/v_2_0/GetObservationTypeTest.java
@@ -0,0 +1,64 @@
+package net.swe.binding.util;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
+
+import org.junit.Test;
+
+import net.opengis.filter.v_2_0.BinaryTemporalOpType;
+import net.opengis.filter.v_2_0.TemporalOpsType;
+import net.opengis.gml.v_3_2_1.TimePeriodType;
+import net.opengis.gml.v_3_2_1.TimePositionType;
+import net.opengis.sos.v_2_0.GetObservationType;
+import net.opengis.sos.v_2_0.GetObservationType.TemporalFilter;
+
+public class GetObservationTypeTest {
+
+	net.opengis.sos.v_2_0.ObjectFactory sosFac = new net.opengis.sos.v_2_0.ObjectFactory();
+	net.opengis.filter.v_2_0.ObjectFactory filterFac = new net.opengis.filter.v_2_0.ObjectFactory(); 
+	net.opengis.gml.v_3_2_1.ObjectFactory gmlFac = new net.opengis.gml.v_3_2_1.ObjectFactory(); 
+		
+
+		/**
+		*  This unit test demonstrates the SOS2 GetObservation temporal filter bindings 
+		*  are incorrect
+		*
+		*  At the time this test was written the temporal filter operations all accepted 
+		*  an arg of type 
+		*
+		*      JAXBElement<TemporalOpType>
+		*
+		* ...but instead should accept an arg of 
+		*
+		*      JAXBElement<* extends TemporalOpType>
+		* 
+		*/
+	@Test
+	public void testDateRangeSelector() throws JAXBException {
+		
+		GetObservationType getObs = new GetObservationType();
+		getObs.setService("SOS");
+		getObs.setVersion("2.0.0");
+
+		TimePeriodType timePeriod = new TimePeriodType();
+		timePeriod.setId("tp1");
+		timePeriod.setBeginPosition(new TimePositionType().withValue("2010-06-25T00:00:00.000Z"));
+		timePeriod.setEndPosition(new TimePositionType().withValue("2010-08-02T00:00:00.000Z"));		
+
+		BinaryTemporalOpType filterParameters = new BinaryTemporalOpType();
+		filterParameters.getExpressionOrAny().add(filterFac.createValueReference("phenomenonTime"));
+		filterParameters.getExpressionOrAny().add(gmlFac.createTimePeriod(timePeriod));
+		JAXBElement<BinaryTemporalOpType> binaryTemporalOps = filterFac.createDuring(filterParameters);
+		
+		TemporalFilter temporalFilter = sosFac.createGetObservationTypeTemporalFilter();
+		//  THIS line does not compile, but should
+		temporalFilter.setTemporalOps(binaryTemporalOps);
+	}
+}
